254a2a766caaaafcf570a991b635d93a4c9c9bb6
omer <omerharuncetin@gmail.com>
2025-07-04 21:42:22 +0200

    sbt contracts for level badges and certificates

---
+++ packages/sbt/src/certificates/A2Certificate.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import {ERC721} from "../../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "../../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract LinguaA2Certificate is ERC721, Ownable {
+    uint256 private _nextTokenId;
+
+    constructor()
+        ERC721("A2 Certificate", "A2Cert")
+        Ownable()
+    {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://hackathon.omerharuncetin.com/api/certificates/a2/";
+    }
+
+    function safeMint(address to) public onlyOwner returns (uint256) {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        return tokenId;
+    }
+
+    // ----------- Disable transfers ------------------
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        override
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+
+        // Only allow minting (from 0) and burning (to 0)
+        if (from != address(0) && to != address(0)) {
+            revert("Soulbound: tokens are non-transferable");
+        }
+    }
+}
